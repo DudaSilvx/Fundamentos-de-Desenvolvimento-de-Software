a158a812b7520923c32bb22a7281370932ff35dc
DudaSilvx <113145263+DudaSilvx@users.noreply.github.com>
2024-09-04 21:18:12 -0300

    Caixa de alerta JS

---
+++ Aula 3/ex01.js
@@ -0,0 +1,12 @@
+/* Declare duas variáveis chamadas "idade" e "peso", atribua valores
+ a elas e imprima os valores no console.
+
+ idade = 21; 
+ peso = 44;
+
+ console.log("Minha idade é "+idade+" anos e o meu peso é "+peso+"kg.");*/
+
+ let idade = prompt("Qual a sua idade?\n");
+ let peso = prompt("Qual o seu peso\n");
+
+ console.log(idade);
